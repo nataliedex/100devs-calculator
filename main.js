b255104dc16d72ca2011febc840695b6a8cc06ef
Natalie Blumen <natalie.blumen@gmail.com>
2024-10-17 12:50:26 -0400

    updated some errors

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function clearValues() {
     firstNumber = "";
     secondNumber = "";
     currentOperation = "";
-    isFirstNumberComputer = false;
+    isFirstNumberComplete = false;
     displayScreen.innerHTML = "";
 }
 
